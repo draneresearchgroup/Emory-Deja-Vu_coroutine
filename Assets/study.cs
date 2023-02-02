79f632c6f6cc07b02920fec2cf9209937d44e9d1
espkz <elliepaek@Ellies-MacBook-Pro.local>
2023-02-01 21:13:41 -0500

    study phase mostly done

--- Assets/study.cs
+++ Assets/study.cs
@@ -7,33 +7,55 @@ using UnityEngine.Video;
 public class study : MonoBehaviour
 {
     public string[] videonames = new string[16];
-    public Video[] videos = new Video[16];
-    private VideoPlayer testVideo;
+    public VideoClip[] videos = new VideoClip[16];
+    private VideoPlayer vp;
+    public GameObject cross;
+    private int index = 0;
+    public Material skyboxMaterial;
+    public Material black;
 
     IEnumerator deja_vu_coroutine(string videoname)
-    { 
-        testVideo = GetComponent<VideoPlayer>();
-        testVideo.Play();
-        yield return new WaitForSeconds(3f); // how long is this going to be played?
-        testVideo.Stop();
+    {
+        vp.Prepare();
+        vp.Play();
+        // redundant but can't erase due to compiling errors?
+        if (vp.isPaused) {
+            //play dark screen
+            //1. Remove VP render new Skybox
+            //2. Switch to Camera Rendered onto specific crosshair (+) image
+            //3. Wait for x amount of time (float)
+            RenderSettings.skybox = (black);
+            cross.SetActive(true);
+            yield return new WaitForSeconds(3f);
+            cross.SetActive(false);
+            playNextVideo();
+        }
+    }
+
+    IEnumerator playNextVideo()
+    {
         //play dark screen
-        RenderSettings.skybox = (null);
         //1. Remove VP render new Skybox
         //2. Switch to Camera Rendered onto specific crosshair (+) image
         //3. Wait for x amount of time (float)
-        yield return new WaitForSeconds(3f);
-        playNextVideo(); //go to onEnd / playNextVideo method
-    }
-
-    void playNextVideo()
-    {
+        RenderSettings.skybox = (black);
+        cross.SetActive(true);
+        yield return new WaitForSeconds(3);
+        cross.SetActive(false);
+        index += 1;
         if (index < 16)
         {
-            string nextvideo = videonames.next();
+            string nextvideo = videonames[index];
+            vp.clip = videos[index];
+            RenderSettings.skybox = skyboxMaterial;
             deja_vu_coroutine(nextvideo);
         }
         else
         {
+            // just to check to make sure thing ended
+            cross.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            cross.SetActive(false);
             //GO back to a home scene that would play the study phase
             //switch scene //we’ll cover this later but you can use the scenemanager unity object from Unity.SceneManagement
         }
@@ -42,12 +64,17 @@ public class study : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        StartCoroutine(deja_vu_coroutine(videonames[0]));
+        vp = GetComponent<VideoPlayer>();
+        vp.clip = videos[index];
+        RenderSettings.skybox = skyboxMaterial;
+        StartCoroutine(deja_vu_coroutine(videonames[index]));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (vp.isPaused) {
+            StartCoroutine(playNextVideo());
+        }
     }
 }
