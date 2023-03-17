16d7661c09faec223f7ffe6ce73372ca07ceffd6
espkz <ellie.paek@gmail.com>
2023-03-17 14:44:45 -0400

    scene management broke oops

--- Assets/testDJVplayer.cs
+++ Assets/testDJVplayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
-using Unity.SceneManagement;
+// using Unity.SceneManagement;
 
 public class testDJVplayer : MonoBehaviour
 {
