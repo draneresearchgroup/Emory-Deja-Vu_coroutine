de48158d28b4c3e88a68a97411db6acec902e96b
espkz <ellie.paek@gmail.com>
2023-03-26 14:59:28 -0400

    cleanup and more testing

--- Assets/studyDJVplayer.cs
+++ Assets/studyDJVplayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
-using UnityEngine.SceneManagement;  //uncommented scene management package
+using UnityEngine.SceneManagement;
 
 public class studyDJVplayer : MonoBehaviour
 {
